508f118062f5c38e0a5f0967b6fdfe8b788f450c
Chetan Nada <call2chetannada@gmail.com>
2021-11-27 23:55:10 +0530

    reduce method, fetch api async await then try catch

---
+++ script2711.js
@@ -0,0 +1,48 @@
+// 10 days of Javascript Tutorial in Hackerrank
+
+// JavaScript Array reduce()
+// Subtract the numbers in the array, starting from the left:
+let numberSubtract = [325, 25, 100];
+let resultSubtract = numberSubtract.reduce((a, b) => a - b);
+console.log(resultSubtract);
+
+// Subtract the numbers in the array, starting from the right:
+let numberSubtractRight = [100, 25, 100];
+let resultSubtractRight = numberSubtractRight.reduceRight((a, b) => a - b);
+console.log(resultSubtractRight);
+
+// // Add all the numbers in the array
+let numberAdd = [10, 20, 40];
+let resultAdd = numberAdd.reduce((a, b) => a + b);
+console.log(resultAdd);
+
+// JavaScript Fetch API to Get Data using async await and try catch
+let apiUrl = "https://jsonplaceholder.typicode.com/todos";
+async function fetchData(url) {
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    getData(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+fetchData(apiUrl);
+function getData(data) {
+    for(let index in data){
+        for(index; index<=5; index++){
+            console.log(`Id: ${data[index].id}, Title: ${data[index].title}`);
+            break;
+        }
+    }
+}
+
+
+// JavaScript Fetch API to Get Data using async await and then catch
+async function getFetchData(url){
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(`Id: ${data[99].id}, Title: ${data[99].title}`))
+    .catch(error => console.log(error))
+}
+getFetchData(apiUrl);
