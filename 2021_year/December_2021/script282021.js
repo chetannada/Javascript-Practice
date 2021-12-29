26a96d7d467a86f2075c3107d02d69cb46df8e42
Chetan Nada <call2chetannada@gmail.com>
2021-12-29 22:53:23 +0530

    Day 14: Scope in Python Hackerrank

---
+++ 2021_year/December_2021/script282021.js
@@ -0,0 +1,13 @@
+// 30 days of Code Tutorial in Hackerrank
+// https://www.hackerrank.com/domains/tutorials/30-days-of-code
+
+// Day 14: Scope in Python
+// # Add your code here
+//     def computeDifference(self):
+//         maxDiff = 0
+//         arr = self.__elements
+//         for i in range(len(arr)):
+//             for j in range(i+1, len(arr)):
+//                 if abs(arr[j] - arr[i]) > maxDiff:
+//                     maxDiff = abs(arr[j] - arr[i])
+//         self.maximumDifference = maxDiff
