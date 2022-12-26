2c460b355194781db3ee1a5ba07d426948b9cbfc
Chetan Nada <call2chetannada@gmail.com>
2022-12-26 13:16:32 +0530

    manipulate the HTML DOM using Javascript

---
+++ 2022_year/December_2022/index.js
@@ -0,0 +1,7 @@
+// Hello world using Javascript 
+// manipulate the HTML DOM using Javascript
+
+const heading = document.createElement("h1");
+heading.innerHTML = "Hello World";
+const root = document.getElementById("root");
+root.appendChild(heading);
