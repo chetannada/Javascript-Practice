63d4518dea46e0dd7f465ce1b0d336016607816d
Chetan Nada <call2chetannada@gmail.com>
2022-12-26 13:50:00 +0530

    manipulate the HTML DOM using React CDN

--- 2022_year/December_2022/index.js
+++ 2022_year/December_2022/index.js
@@ -1,7 +1,18 @@
 // Hello world using Javascript 
 // manipulate the HTML DOM using Javascript
 
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello World";
-const root = document.getElementById("root");
-root.appendChild(heading);
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello World";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+// manipulate the HTML DOM using React
+
+const heading = React.createElement("h1", {
+    class: "title",
+    style: {color: "red", backgroundColor: "lightgreen"},
+}, "Namaste Everyone");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
