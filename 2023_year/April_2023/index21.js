620d237cbdeca19a4c7445776dc4e342bfea5b58
Chetan Nada <call2chetannada@gmail.com>
2023-04-21 09:50:50 +0530

    Leetcode Solution of To Lower Case

---
+++ 2023_year/April_2023/index21.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/to-lower-case/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase(); // convert string to lowercase using toLowerCase()
+};
+
+/*
+Example 1:
+Input: s = "Hello"
+Output: "hello"
+
+Example 2:
+Input: s = "here"
+Output: "here"
+
+Example 3:
+Input: s = "LOVELY"
+Output: "lovely"
+*/
