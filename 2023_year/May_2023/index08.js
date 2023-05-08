454bce3033a5e3b5701ad985140c8bb820f86725
Chetan Nada <call2chetannada@gmail.com>
2023-05-08 22:31:43 +0530

    Leetcode Solution of Reverse String

---
+++ 2023_year/May_2023/index08.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/reverse-string/
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    s= s.reverse(""); // return reverse String
+};
+
+/*
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+*/
