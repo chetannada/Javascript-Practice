f3a170ceb8555680a9d992427eee8822226986a0
Chetan Nada <call2chetannada@gmail.com>
2023-06-18 11:18:56 +0530

    Leetcode Solution of Neither Minimum nor Maximum

---
+++ 2023_year/June_2023/index18.js
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/neither-minimum-nor-maximum/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNonMinOrMax = function(nums) {
+    if(nums.length <= 2) return -1; // if nums array length is less than or equal to 2 then return -1
+    nums.sort((a,b) => a-b); // sort elements of array nums
+    return nums[1]; // return index 1 element of array nums
+};
+
+/*
+Given an integer array nums containing distinct positive integers, find and return any number from the array that is neither the minimum nor the maximum value in the array, or -1 if there is no such number.
+
+Return the selected integer.
+*/
+
+/*
+Example 1:
+Input: nums = [3,2,1,4]
+Output: 2
+Explanation: In this example, the minimum value is 1 and the maximum value is 4. Therefore, either 2 or 3 can be valid answers.
+
+Example 2:
+Input: nums = [1,2]
+Output: -1
+Explanation: Since there is no number in nums that is neither the maximum nor the minimum, we cannot select a number that satisfies the given condition. Therefore, there is no answer.
+
+Example 3:
+Input: nums = [2,1,3]
+Output: 2
+Explanation: Since 2 is neither the maximum nor the minimum value in nums, it is the only valid answer. 
+ 
+Constraints:
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+All values in nums are distinct
+*/
