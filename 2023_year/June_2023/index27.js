d37ac1d814c582e6bf6777213f38a667dd341381
Chetan Nada <call2chetannada@gmail.com>
2023-06-27 21:15:48 +0530

    Leetcode Solution of Merge Two 2D Arrays by Summing Values

---
+++ 2023_year/June_2023/index27.js
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values/
+
+/**
+ * @param {number[][]} nums1
+ * @param {number[][]} nums2
+ * @return {number[][]}
+ */
+var mergeArrays = function (nums1, nums2) {
+    const numsMap = {}; // initialize empty object numsMap
+      nums1.forEach(([id, num]) => { // loop through the every element of array and store id as key and value as num in numsMap
+          numsMap[id] = num;
+      });
+      nums2.forEach(([id, num]) => { // loop through the every element of array and store id as key and value as numsMap[id] if there then add neighter add 0 to num in numsMap
+          numsMap[id] = (numsMap[id] ?? 0) + num;
+      });
+  
+      return Object.entries(numsMap); // return array of array from object numsMap using Object.entries()
+  };
+
+  
+/*
+You are given two 2D integer arrays nums1 and nums2.
+
+nums1[i] = [idi, vali] indicate that the number with the id idi has a value equal to vali.
+nums2[i] = [idi, vali] indicate that the number with the id idi has a value equal to vali.
+Each array contains unique ids and is sorted in ascending order by id.
+
+Merge the two arrays into one array that is sorted in ascending order by id, respecting the following conditions:
+
+Only ids that appear in at least one of the two arrays should be included in the resulting array.
+Each id should be included only once and its value should be the sum of the values of this id in the two arrays. If the id does not exist in one of the two arrays then its value in that array is considered to be 0.
+Return the resulting array. The returned array must be sorted in ascending order by id.
+*/
+
+/*
+Example 1:
+Input: nums1 = [[1,2],[2,3],[4,5]], nums2 = [[1,4],[3,2],[4,1]]
+Output: [[1,6],[2,3],[3,2],[4,6]]
+Explanation: The resulting array contains the following:
+- id = 1, the value of this id is 2 + 4 = 6.
+- id = 2, the value of this id is 3.
+- id = 3, the value of this id is 2.
+- id = 4, the value of this id is 5 + 1 = 6.
+
+Example 2:
+Input: nums1 = [[2,4],[3,6],[5,5]], nums2 = [[1,3],[4,3]]
+Output: [[1,3],[2,4],[3,6],[4,3],[5,5]]
+Explanation: There are no common ids, so we just include each id with its value in the resulting list.
+ 
+Constraints:
+1 <= nums1.length, nums2.length <= 200
+nums1[i].length == nums2[j].length == 2
+1 <= idi, vali <= 1000
+Both arrays contain unique ids.
+Both arrays are in strictly ascending order by id.
+*/
