887fa77823ab8d98855608633c78a2df30c49adb
Chetan Nada <call2chetannada@gmail.com>
2023-07-24 21:23:23 +0530

    Leetcode Solution of Sort Array by Increasing Frequency

---
+++ 2023_year/July_2023/index24.js
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/sort-array-by-increasing-frequency/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var frequencySort = function (nums) {
+    let map = new Map(); // initialize a map using Map()
+    for (let element of nums) { // loop through the every element of array nums
+        map.set(element, (map.get(element) + 1) || 1); // if map has element then increment it by 1 or set to 1
+    }
+    return nums.sort((a, b) => map.get(a) - map.get(b) || b - a) // return the sort array of nums if multiple values have same frequency then sort by array items in decreasing order ( b - a) or by map item (map.get(a) - map.get(b))
+};
+
+/*
+Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+
+Return the sorted array.
+/*
+
+
+/*
+Example 1:
+Input: nums = [1,1,2,2,2,3]
+Output: [3,1,1,2,2,2]
+Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
+
+Example 2:
+Input: nums = [2,3,1,3,2]
+Output: [1,3,3,2,2]
+Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
+
+Example 3:
+Input: nums = [-1,1,-6,4,5,-6,1,4,1]
+Output: [5,-1,4,4,-6,-6,1,1,1]
+ 
+Constraints:
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100
+/*
