08f82dcb86a229a316c378e971dbb0a1d44a9147
Chetan Nada <call2chetannada@gmail.com>
2023-08-05 20:51:13 +0530

    Leetcode Solution of Word Break

---
+++ 2023_year/August_2023/index05.js
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/word-break/
+
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+var wordBreak = function (s, wordDict) {
+    let arr = new Array(s.length + 1).fill(false);
+
+    arr[0] = true;
+
+    for (let i = 0; i <= s.length; i++) {
+        if (arr[i] === true) {
+            for (let word of wordDict) {
+                if (s.slice(i, i + word.length) === word) {
+                    arr[i + word.length] = true;
+                }
+            }
+        }
+    }
+    return arr[s.length];
+};
+
+/*
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+*/
+
+/*
+Example 1:
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Example 3:
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+ 
+Constraints:
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+*/
