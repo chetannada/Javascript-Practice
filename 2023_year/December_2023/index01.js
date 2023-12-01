eddb816edccff9a40bb831ebc812b6434978281f
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2023-12-01 20:48:12 +0530

    Leetcode Slution of Sort Characters By Frequency

---
+++ 2023_year/December_2023/index01.js
@@ -0,0 +1,54 @@
+// https://leetcode.com/problems/sort-characters-by-frequency/description/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function (s) {
+    const map = new Map();
+
+    for (const letter of s) {
+        map.set(letter, (map.get(letter) ?? 0) + 1);
+    }
+
+    return [...map].sort((a, b) => b[1] - a[1])
+        .reduce((acc, [letter, freq]) => acc + letter.repeat(freq), '');
+};
+
+/*
+Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
+
+Return the sorted string. If there are multiple answers, return any of them.
+*/
+
+
+/*
+Example 1:
+
+Input: s = "tree"
+Output: "eert"
+Explanation: 'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+
+Example 2:
+
+Input: s = "cccaaa"
+Output: "aaaccc"
+Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+Note that "cacaca" is incorrect, as the same characters must be together.
+
+Example 3:
+
+Input: s = "Aabb"
+Output: "bbAa"
+Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+*/
+
+
+/*
+Constraints:
+
+1 <= s.length <= 5 * 105
+s consists of uppercase and lowercase English letters and digits.
+*/
