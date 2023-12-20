faf8f7d9351c88f7ee00c2e0817ce1045921d97e
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2023-12-20 12:12:01 +0530

    Leetcode Solution of Ways to Make a Fair Array

---
+++ 2023_year/December_2023/index20.js
@@ -0,0 +1,82 @@
+// https://leetcode.com/problems/ways-to-make-a-fair-array/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var waysToMakeFair = function (nums) {
+    let odds = 0;
+    let evens = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2) odds += nums[i];
+        else evens += nums[i];
+    }
+
+    let preOdds = 0;
+    let preEvens = 0;
+    let postOdds = odds;
+    let postEvens = evens;
+    let totInd = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i % 2) postOdds -= nums[i];
+        else postEvens -= nums[i];
+
+        if (preEvens + postOdds === preOdds + postEvens) {
+            totInd++;
+        }
+
+        if (i % 2) preOdds += nums[i];
+        else preEvens += nums[i];
+    }
+
+    return totInd;
+};
+
+/*
+You are given an integer array nums. You can choose exactly one index (0-indexed) and remove the element. Notice that the index of the elements may change after the removal.
+
+For example, if nums = [6,1,7,4,1]:
+
+Choosing to remove index 1 results in nums = [6,7,4,1].
+Choosing to remove index 2 results in nums = [6,1,4,1].
+Choosing to remove index 4 results in nums = [6,1,7,4].
+An array is fair if the sum of the odd-indexed values equals the sum of the even-indexed values.
+
+Return the number of indices that you could choose such that after the removal, nums is fair.
+*/
+
+
+/*
+Example 1:
+
+Input: nums = [2,1,6,4]
+Output: 1
+Explanation:
+Remove index 0: [1,6,4] -> Even sum: 1 + 4 = 5. Odd sum: 6. Not fair.
+Remove index 1: [2,6,4] -> Even sum: 2 + 4 = 6. Odd sum: 6. Fair.
+Remove index 2: [2,1,4] -> Even sum: 2 + 4 = 6. Odd sum: 1. Not fair.
+Remove index 3: [2,1,6] -> Even sum: 2 + 6 = 8. Odd sum: 1. Not fair.
+There is 1 index that you can remove to make nums fair.
+
+Example 2:
+
+Input: nums = [1,1,1]
+Output: 3
+Explanation: You can remove any index and the remaining array is fair.
+
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+Explanation: You cannot make a fair array after removing any index.
+*/
+
+
+/*
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+*/
