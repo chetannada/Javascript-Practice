bf84428335b687951d24fc605f8370c0234d3fea
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2023-12-22 10:16:15 +0530

    Leetcode Solution of Combination Sum

---
+++ 2023_year/December_2023/index22.js
@@ -0,0 +1,72 @@
+// https://leetcode.com/problems/combination-sum/submissions/1125052299/
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (candidates, target) {
+    let result = [];
+
+    dfs(candidates, target, [], result);
+
+    return result;
+};
+
+function dfs(nums, target, path, result) {
+
+    if (target < 0) {
+        return;
+    }
+
+    if (target === 0) {
+        result.push(path);
+        return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        dfs(nums.slice(i), target - nums[i], [...path, nums[i]], result);
+    }
+}
+
+/*
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the 
+frequency
+ of at least one of the chosen numbers is different.
+
+The test cases are generated such that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+*/
+
+
+/*
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+*/
+
+
+/*
+Constraints:
+
+1 <= candidates.length <= 30
+2 <= candidates[i] <= 40
+All elements of candidates are distinct.
+1 <= target <= 40
+*/
