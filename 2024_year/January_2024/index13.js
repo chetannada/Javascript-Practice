242f3a10206acc9597a80189b55753cfd037cd8e
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2024-01-13 21:51:34 +0530

    Leetcode Solution of Find Words Containing Character

---
+++ 2024_year/January_2024/index13.js
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/find-words-containing-character/description/
+
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function (words, x) {
+    let result = [];
+
+    // if x is includes in any element of array words then add that index in array result
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].includes(x)) result.push(i);
+    }
+
+    // return the array result
+    return result;
+};
+
+/*
+You are given a 0-indexed array of strings words and a character x.
+
+Return an array of indices representing the words that contain the character x.
+
+Note that the returned array may be in any order.
+*/
+
+
+/*
+Example 1:
+
+Input: words = ["leet","code"], x = "e"
+Output: [0,1]
+Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+
+Example 2:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+Output: [0,2]
+Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+
+Example 3:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+Output: []
+Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+*/
+
+
+/*
+Constraints:
+
+1 <= words.length <= 50
+1 <= words[i].length <= 50
+x is a lowercase English letter.
+words[i] consists only of lowercase English letters.
+*/
