724889a9d1cf474ab18084d6bdb5160640ab491b
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2024-02-28 23:08:13 +0530

    Leetcode Soluation of Move Zeroes

---
+++ 2024_year/February_2024/index28.js
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/move-zeroes/
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+
+    // initialize nonZero to 0
+    let nonZero = 0;
+
+    // loop through the every element of array nums
+    for (let i = 0; i < nums.length; i++) {
+
+        // initialize current to be ith element of nums
+        let current = nums[i];
+
+        // make every ith element of nums to be 0
+        nums[i] = 0;
+
+        // if current is not eqaul to 0
+        if (current !== 0) {
+
+            // assign current to noZeroth element of nums and increment nonZero
+            nums[nonZero] = current;
+            nonZero++;
+        }
+    }
+
+};
+
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+*/
+
+
+/*
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+*/
+
+
+/*
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+*/
