9f4b0c1ad3dc4a43caaa780b7e4eb9bb62f1c5c6
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2024-03-15 21:18:41 +0530

    Leetcode Solution of Sort the Students by Their Kth Score

---
+++ 2024_year/March_2024/index15.js
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/sort-the-students-by-their-kth-score/description/
+
+/**
+ * @param {number[][]} score
+ * @param {number} k
+ * @return {number[][]}
+ */
+var sortTheStudents = function (score, k) {
+
+    // sort the m  x n matrix in descending order based on kth index in every row
+    score.sort((a, b) => b[k] - a[k]);
+
+    // return answer score
+    return score;
+};
+
+/*
+There is a class with m students and n exams. You are given a 0-indexed m x n integer matrix score, where each row represents one student and score[i][j] denotes the score the ith student got in the jth exam. The matrix score contains distinct integers only.
+
+You are also given an integer k. Sort the students (i.e., the rows of the matrix) by their scores in the kth (0-indexed) exam from the highest to the lowest.
+
+Return the matrix after sorting it.
+*/
+
+
+/*
+Example 1:
+
+Input: score = [[10,6,9,1],[7,5,11,2],[4,8,3,15]], k = 2
+Output: [[7,5,11,2],[10,6,9,1],[4,8,3,15]]
+Explanation: In the above diagram, S denotes the student, while E denotes the exam.
+- The student with index 1 scored 11 in exam 2, which is the highest score, so they got first place.
+- The student with index 0 scored 9 in exam 2, which is the second highest score, so they got second place.
+- The student with index 2 scored 3 in exam 2, which is the lowest score, so they got third place.
+
+Example 2:
+
+Input: score = [[3,4],[5,6]], k = 0
+Output: [[5,6],[3,4]]
+Explanation: In the above diagram, S denotes the student, while E denotes the exam.
+- The student with index 1 scored 5 in exam 0, which is the highest score, so they got first place.
+- The student with index 0 scored 3 in exam 0, which is the lowest score, so they got second place.
+*/
+
+
+/*
+Constraints:
+
+m == score.length
+n == score[i].length
+1 <= m, n <= 250
+1 <= score[i][j] <= 105
+score consists of distinct integers.
+0 <= k < n
+*/
