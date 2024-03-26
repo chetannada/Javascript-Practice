f84706627015c0944cf2b6b6503f956db6ca55e6
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2024-03-26 22:39:12 +0530

    Leetcode Solution of Find All Duplicates in an Array

---
+++ 2024_year/March_2024/index26.js
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDuplicates = function (nums) {
+
+    // initialize answer to be empty array
+    let ans = [];
+
+    // add first value 0 in array nums
+    nums.unshift(0);
+
+    // loop through every element of arrya nums
+    for (let i = 0; i < nums.length; i++) {
+
+        // find absoulute value of current element of nums
+        let index = Math.abs(nums[i]);
+
+        // if indexth element of nums is less than 0 then add index in array ans
+        if (nums[index] < 0) ans.push(index);
+
+        // make indexth element of nums to negative
+        nums[index] *= (-1);
+    }
+
+    // return array ans
+    return ans;
+};
+
+/*
+Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+You must write an algorithm that runs in O(n) time and uses only constant extra space.
+*/
+
+
+/*
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [2,3]
+
+Example 2:
+
+Input: nums = [1,1,2]
+Output: [1]
+
+Example 3:
+
+Input: nums = [1]
+Output: []
+*/
+
+
+/*
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+Each element in nums appears once or twice.
+*/
