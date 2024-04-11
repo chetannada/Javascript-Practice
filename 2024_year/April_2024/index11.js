a3060df61beb6778b9e279dab06d0b9f977193a3
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2024-04-11 23:20:42 +0530

    Leetcode Solution of Sorting the Sentence

---
+++ 2024_year/April_2024/index11.js
@@ -0,0 +1,63 @@
+// https://leetcode.com/problems/sorting-the-sentence/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortSentence = function (s) {
+
+    // convert string s to array using split(' ')
+    // n equal to length of array
+    // initialize new array equal to length n and fill with 0
+    let arr = s.split(' '), n = arr.length, result = new Array(n).fill(0);
+
+    // run for of loop of array arr
+    for (let char of arr) {
+
+        // initialize len equal to length of char - 1
+        let len = char.length - 1;
+
+        // find position of words in char
+        let index = char[len];
+
+        // remove last character from char and assign to result with index - 1
+        result[index - 1] = char.slice(0, len);
+    }
+
+    // convert array to string and return it
+    return result.join(' ');
+};
+
+/*
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+
+A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+
+For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+Given a shuffled sentence s containing no more than 9 words, reconstruct and return the original sentence.
+*/
+
+
+/*
+Example 1:
+
+Input: s = "is2 sentence4 This1 a3"
+Output: "This is a sentence"
+Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
+Example 2:
+
+Input: s = "Myself2 Me1 I4 and3"
+Output: "Me Myself and I"
+Explanation: Sort the words in s to their original positions "Me1 Myself2 and3 I4", then remove the numbers.
+*/
+
+
+/*
+Constraints:
+
+2 <= s.length <= 200
+s consists of lowercase and uppercase English letters, spaces, and digits from 1 to 9.
+The number of words in s is between 1 and 9.
+The words in s are separated by a single space.
+s contains no leading or trailing spaces.
+*/
