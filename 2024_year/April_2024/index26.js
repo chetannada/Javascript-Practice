7ac46ac16924aa188bb760633903fdfb6ec7f03c
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2024-04-26 20:50:20 +0530

    Leetcode Solution of To Lower Case

---
+++ 2024_year/April_2024/index26.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/to-lower-case/
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function (s) {
+    // convert to lowercase and return the string
+    return s.toLowerCase();
+};
+
+/*
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+*/
+
+
+/*
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+*/
+
+
+/*
+Constraints:
+
+1 <= s.length <= 100
+s consists of printable ASCII characters.
+*/
