312913618d7b87130cd568c3fc6ab40aa5c9f0ff
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2024-06-07 21:41:33 +0530

    Leetcode Solution of Replace Words

---
+++ 2024_year/June_2024/index07.js
@@ -0,0 +1,64 @@
+// https://leetcode.com/problems/replace-words/description/
+
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
+var replaceWords = function (dictionary, sentence) {
+
+    // convert sentence into an array
+    let result = sentence.split(" ");
+
+    // run for of loop for dictionary
+    for (let root of dictionary) {
+
+        // run for loop from index 0 to length of result
+        for (let i = 0; i < result.length; i++) {
+
+            // if result[i] is start with root then assign root to result[i]
+            if (result[i].startsWith(root)) {
+                result[i] = root;
+            }
+        }
+    }
+
+    // convert array to string and return it
+    return result.join(" ");
+};
+
+/*
+In English, we have a concept called root, which can be followed by some other word to form another longer word - let's call this word derivative. For example, when the root "help" is followed by the word "ful", we can form a derivative "helpful".
+
+Given a dictionary consisting of many roots and a sentence consisting of words separated by spaces, replace all the derivatives in the sentence with the root forming it. If a derivative can be replaced by more than one root, replace it with the root that has the shortest length.
+
+Return the sentence after the replacement.
+*/
+
+
+/*
+Example 1:
+
+Input: dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
+Output: "the cat was rat by the bat"
+
+Example 2:
+
+Input: dictionary = ["a","b","c"], sentence = "aadsfasf absbs bbab cadsfafs"
+Output: "a a b c"
+*/
+
+
+/*
+Constraints:
+
+1 <= dictionary.length <= 1000
+1 <= dictionary[i].length <= 100
+dictionary[i] consists of only lower-case letters.
+1 <= sentence.length <= 106
+sentence consists of only lower-case letters and spaces.
+The number of words in sentence is in the range [1, 1000]
+The length of each word in sentence is in the range [1, 1000]
+Every two consecutive words in sentence will be separated by exactly one space.
+sentence does not have leading or trailing spaces.
+*/
