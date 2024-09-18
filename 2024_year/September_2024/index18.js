db0ce85bacf7e32c2c048cdfff155e8c667ad640
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2024-09-18 21:52:40 +0530

    Leetcode Solution of Range Sum Query - Immutable

---
+++ 2024_year/September_2024/index18.js
@@ -0,0 +1,77 @@
+// https://leetcode.com/problems/range-sum-query-immutable/description/
+
+
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+
+    // initialize arr as nums
+    this.arr = nums;
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+
+    // initialize sum as 0
+    let sum = 0;
+
+    // run for loop from left to right (inclusive)
+    for (let i = left; i <= right; i++) {
+
+        // add ith element of arr to sum
+        sum += this.arr[i];
+    }
+
+    // return final answer sum
+    return sum;
+};
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
+
+
+
+/*
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+*/
+
+
+/*
+Example 1:
+
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+*/
+
+
+/*
+Constraints:
+
+1 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= left <= right < nums.length
+At most 104 calls will be made to sumRange.
+*/
