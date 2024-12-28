09158282832df633b1fcd41520b8210989d03e08
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2024-12-28 15:42:20 +0530

    Leetcode Solution of Contains Duplicate

---
+++ 2024_year/December_2024/index13.js
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/contains-duplicate/description/
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+
+    // initialize set to find unique element in array
+    let set = new Set(nums);
+
+    // if size of set is equal to length of nums then return false otherwise true
+    return set.size === nums.length ? false : true;
+};
+
+
+/*
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+*/
+
+
+/*
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+*/
+
+
+/*
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
