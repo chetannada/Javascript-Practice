f40013527603a5d365679333733a874c06aa8536
Chetan Nada <83969719+chetannada@users.noreply.github.com>
2024-12-28 15:26:11 +0530

    Leetcode Solution of Single Number

---
+++ 2024_year/December_2024/index11.js
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/single-number/description/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+
+    // initialize result as first element of nums
+    let result = nums.at(0);
+
+    for (let i = 1; i < nums.length; i++) {
+
+        // do XOR of every element of nums with result
+        // Hint: XOR of same number will gives 0
+        result ^= nums[i];
+    }
+
+    // return final answer result
+    return result;
+};
+
+
+/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+*/
+
+
+/*
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+Output: 1
+*/
+
+
+/*
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+*/
